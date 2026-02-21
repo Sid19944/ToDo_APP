6dcba04ce24bf6ab3e4991963fc49351297399dd
Siddharth Sarkar <sidsarkar2112@gmail.com>
2026-02-21 13:38:55 +0530

    fix double showing li

--- frontend/src/pages/task/AllTodo.jsx
+++ frontend/src/pages/task/AllTodo.jsx
@@ -107,13 +107,16 @@ function AllTodo({ next }) {
             </span>
           </h1>
           <ul className="flex flex-col gap-1">
-            {tasks?.map((task) => {
-              let newdate = new Date(task.todoDate).toLocaleDateString("en-IN");
-              if (dates.includes(newdate)) return;
-              dates.push(newdate);
+            {filter == "today"
+              ? tasks?.map((task) => {
+                  let newdate = new Date(task.todoDate).toLocaleDateString(
+                    "en-IN",
+                  );
+                  if (dates.includes(newdate)) return;
+                  dates.push(newdate);
 
-              return filter == "today"
-                ? date?.toLocaleDateString("en-IN") == newdate &&
+                  return (
+                    date?.toLocaleDateString("en-IN") == newdate &&
                     tasks
                       .filter((task) => {
                         const viewData = new Date(
@@ -256,140 +259,137 @@ function AllTodo({ next }) {
                           </li>
                         );
                       })
-                : tasks.map((task) => {
-                    return (
-                      <li
-                        className="bg-gray-600 p-2 rounded-lg flex items-center flex-col gap-1"
-                        key={task._id}
-                      >
-                        <div className="w-full flex items-center justify-between p-1 rounded-lg bg-gray-400">
-                          <span className="flex gap-1 items-center">
-                            <span className="flex">
-                              <input
-                                type="checkbox"
-                                className="w-5 h-4 accent-purple-600"
-                                checked={task.isCompleted}
-                                value={!task.isCompleted}
-                                onChange={(e) =>
-                                  handleTaskDone(e.target.value, task._id)
-                                }
-                              />
-                            </span>
-                            <span
-                              className={`font-semibold ${task?.isCompleted && "line-through opacity-70"} ${task.category == "Work" ? "text-purple-600" : "text-yellow-400"}`}
-                            >
-                              {task.title}
-                            </span>
+                  );
+                })
+              : tasks.map((task) => {
+                  return (
+                    <li
+                      className="bg-gray-600 p-2 rounded-lg flex items-center flex-col gap-1"
+                      key={task._id}
+                    >
+                      <div className="w-full flex items-center justify-between p-1 rounded-lg bg-gray-400">
+                        <span className="flex gap-1 items-center">
+                          <span className="flex">
+                            <input
+                              type="checkbox"
+                              className="w-5 h-4 accent-purple-600"
+                              checked={task.isCompleted}
+                              value={!task.isCompleted}
+                              onChange={(e) =>
+                                handleTaskDone(e.target.value, task._id)
+                              }
+                            />
                           </span>
-                          <span className="flex gap-1">
-                            {task?.subTask?.length != 0 &&
-                              (showSubList == task._id ? (
-                                <Link onClick={() => setShowSubList("")}>
-                                  <ArrowDropUpIcon />
-                                </Link>
-                              ) : (
-                                <Link onClick={() => setShowSubList(task._id)}>
-                                  <ArrowDropDownIcon />
-                                </Link>
-                              ))}
-
-                            <Link
-                              className="active:text-red-600 hover:text-red-600"
-                              onClick={() => handleTaskDelete(task._id)}
-                            >
-                              <DeleteForeverIcon />
-                            </Link>
-
-                            <Link
-                              className="active:text-green-600 hover:text-green-600"
-                              onClick={(e) => goEditTask(e, task._id)}
-                            >
-                              <EditDocumentIcon />
-                            </Link>
-                            <Link
-                              className="active:text-blue-600  hover:text-blue-600"
-                              onClick={(e) => goAddSubTask(e, task._id)}
-                            >
-                              <AddCircleOutlineIcon />
-                            </Link>
-                          </span>
-                        </div>
-                        <div
-                          className={`w-full bg-gray-400 px-2 rounded-lg ${task.isCompleted && "line-through"}`}
-                        >
-                          {task.description}
-                        </div>
-
-                        {showSubList == task._id && (
-                          <ol className="w-[98%] flex flex-col gap-1 ">
-                            <span className="text-sm text-blue-200">
-                              Subtasks
-                            </span>
-                            {task?.subTask?.map((sub) => (
-                              <li
-                                key={sub._id}
-                                className="border px-1 rounded-lg flex w-full justify-between bg-gray-500 items-center flex-wrap gap-2 p-1"
-                              >
-                                <div className="flex justify-between w-full  rounded-lg px-2 bg-gray-400">
-                                  <div className="flex gap-1 items-center">
-                                    <input
-                                      type="checkbox"
-                                      className="w-5 h-4 accent-purple-600"
-                                      checked={sub.isCompleted}
-                                      value={!sub.isCompleted}
-                                      onChange={(e) =>
-                                        handleSubTaskDone(
-                                          e.target.value,
-                                          sub._id,
-                                        )
-                                      }
-                                    />
-                                    <span
-                                      className={`${sub.isCompleted && "line-through"} ${task.category == "Work" ? "text-purple-600" : "text-yellow-400"}`}
-                                    >
-                                      {sub.title}
-                                    </span>
-                                  </div>
-                                  <div className="flex gap-2">
-                                    <Link
-                                      className="active:text-red-600 hover:text-red-600"
-                                      onClick={() =>
-                                        handleSubTaskDelete(sub._id)
-                                      }
-                                    >
-                                      <DeleteForeverIcon />
-                                    </Link>
-                                    <Link
-                                      className="active:text-green-600 hover:text-green-600"
-                                      onClick={(e) =>
-                                        goEditSubTask(e, sub._id, task._id)
-                                      }
-                                    >
-                                      <EditDocumentIcon />
-                                    </Link>
-                                  </div>
-                                </div>
-                                <div
-                                  className={`w-full bg-gray-400 px-2 rounded-lg ${sub.isCompleted && "line-through"}`}
-                                >
-                                  {sub.description}
-                                </div>
-                              </li>
-                            ))}
-                          </ol>
-                        )}
-                        <span
-                          className={`w-full flex items-center gap-1 ${task.priority == "low" && "text-yellow-300 "} ${task.priority == "medium" && "text-green-300"} ${task.priority == "high" && "text-red-500"}`}
-                        >
                           <span
-                            className={`w-2 h-2 rounded-[100%] ${task.priority == "low" && "bg-yellow-300 "} ${task.priority == "medium" && "bg-green-300"} ${task.priority == "high" && "bg-red-500"} `}
-                          ></span>
-                          {task.priority}
+                            className={`font-semibold ${task?.isCompleted && "line-through opacity-70"} ${task.category == "Work" ? "text-purple-600" : "text-yellow-400"}`}
+                          >
+                            {task.title}
+                          </span>
                         </span>
-                      </li>
-                    );
-                  });
-            })}
+                        <span className="flex gap-1">
+                          {task?.subTask?.length != 0 &&
+                            (showSubList == task._id ? (
+                              <Link onClick={() => setShowSubList("")}>
+                                <ArrowDropUpIcon />
+                              </Link>
+                            ) : (
+                              <Link onClick={() => setShowSubList(task._id)}>
+                                <ArrowDropDownIcon />
+                              </Link>
+                            ))}
+
+                          <Link
+                            className="active:text-red-600 hover:text-red-600"
+                            onClick={() => handleTaskDelete(task._id)}
+                          >
+                            <DeleteForeverIcon />
+                          </Link>
+
+                          <Link
+                            className="active:text-green-600 hover:text-green-600"
+                            onClick={(e) => goEditTask(e, task._id)}
+                          >
+                            <EditDocumentIcon />
+                          </Link>
+                          <Link
+                            className="active:text-blue-600  hover:text-blue-600"
+                            onClick={(e) => goAddSubTask(e, task._id)}
+                          >
+                            <AddCircleOutlineIcon />
+                          </Link>
+                        </span>
+                      </div>
+                      <div
+                        className={`w-full bg-gray-400 px-2 rounded-lg ${task.isCompleted && "line-through"}`}
+                      >
+                        {task.description}
+                      </div>
+
+                      {showSubList == task._id && (
+                        <ol className="w-[98%] flex flex-col gap-1 ">
+                          <span className="text-sm text-blue-200">
+                            Subtasks
+                          </span>
+                          {task?.subTask?.map((sub) => (
+                            <li
+                              key={sub._id}
+                              className="border px-1 rounded-lg flex w-full justify-between bg-gray-500 items-center flex-wrap gap-2 p-1"
+                            >
+                              <div className="flex justify-between w-full  rounded-lg px-2 bg-gray-400">
+                                <div className="flex gap-1 items-center">
+                                  <input
+                                    type="checkbox"
+                                    className="w-5 h-4 accent-purple-600"
+                                    checked={sub.isCompleted}
+                                    value={!sub.isCompleted}
+                                    onChange={(e) =>
+                                      handleSubTaskDone(e.target.value, sub._id)
+                                    }
+                                  />
+                                  <span
+                                    className={`${sub.isCompleted && "line-through"} ${task.category == "Work" ? "text-purple-600" : "text-yellow-400"}`}
+                                  >
+                                    {sub.title}
+                                  </span>
+                                </div>
+                                <div className="flex gap-2">
+                                  <Link
+                                    className="active:text-red-600 hover:text-red-600"
+                                    onClick={() => handleSubTaskDelete(sub._id)}
+                                  >
+                                    <DeleteForeverIcon />
+                                  </Link>
+                                  <Link
+                                    className="active:text-green-600 hover:text-green-600"
+                                    onClick={(e) =>
+                                      goEditSubTask(e, sub._id, task._id)
+                                    }
+                                  >
+                                    <EditDocumentIcon />
+                                  </Link>
+                                </div>
+                              </div>
+                              <div
+                                className={`w-full bg-gray-400 px-2 rounded-lg ${sub.isCompleted && "line-through"}`}
+                              >
+                                {sub.description}
+                              </div>
+                            </li>
+                          ))}
+                        </ol>
+                      )}
+                      <span
+                        className={`w-full flex items-center gap-1 ${task.priority == "low" && "text-yellow-300 "} ${task.priority == "medium" && "text-green-300"} ${task.priority == "high" && "text-red-500"}`}
+                      >
+                        <span
+                          className={`w-2 h-2 rounded-[100%] ${task.priority == "low" && "bg-yellow-300 "} ${task.priority == "medium" && "bg-green-300"} ${task.priority == "high" && "bg-red-500"} `}
+                        ></span>
+                        {task.priority}
+                      </span>
+                    </li>
+                  );
+                })}
+
             <div
               id="add-task"
               className={`border my-4 p-2 rounded-lg cursor-pointer gap-1 flex items-center ${showPage == "add-task" ? "bg-blue-400" : "bg-gray-400"}`}
